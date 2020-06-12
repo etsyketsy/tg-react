a321eaa3d6fee70307e757227adf3e5b5e0651f7
Sarah Rabah <sarahmrabah@gmail.com>
2020-06-12 10:53:20 -0400

    Styled list with desription and track listing for releases

--- src/containers/ReleaseTile/index.js
+++ src/containers/ReleaseTile/index.js
@@ -5,24 +5,29 @@ import './index.css';
 class ReleaseTile extends Component {
 
     render() {
-        console.log(this.props.item)
         return (
             <div id="releaseTile">
                 <img src={this.props.item.image}
                     alt='releaseCover'
                 />
-                <div className='details'>
-                    <div className='cat_num'>
-                        {this.props.item.cat_num}
+                <div id='releaseDetails'>
+                    <div id='releaseHeadline'>
+                        <div id='cat_num'>
+                            {this.props.item.cat_num}:
+                        </div>
+                        <div id='releaseArtist'>
+                            {this.props.item.artist} - 
+                        </div>
+                        <div id='releaseTitle'>
+                            {this.props.item.release_title}
+                        </div>
                     </div>
-                    <div className='releaseArtist'>
-                        {this.props.item.artist}
-                    </div>
-                    <div className='releaseTitle'>
-                        {this.props.item.release_title}
-                    </div>
-                    <div className='bio'>
+                    <div id='releaseBio'>
                         {ReactHtmlParser(this.props.item.bio)}
+                    </div>
+                    <div className='tracks'>
+                        <div id='tracksHeader'>Tracks</div>
+                        {ReactHtmlParser(this.props.item.tracklisting)}
                     </div>
                 </div>
 
