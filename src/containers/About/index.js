f9d78536b2707d0d70fef8a9c26cc76d5c9e15ad
Sarah Rabah <sarahmrabah@gmail.com>
2020-06-13 21:49:02 -0400

    jihad's saturday afternoon work

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -9,13 +9,11 @@ class About extends Component {
             <div id='about'>
                 <div className="sectionHeader">// About Twelve Gauge</div>
                 <div className="aboutDisplay">
-                    Twelve Gauge Records is an independent record label currently operating out of New York, NY with a rich roster of hardcore and metal artists like Sabertooth Zombie, Power, Heavy Heavy Low Low, and others.
-          <br></br><br></br>
-                    The label was founded in 2004 with it's first release, Courtside Seats EP by Heavy Heavy Low Low, out in early 2005. Since then, over 20 artists put together a discography of over 30 releases mostly pressed on vinyl only.
-          <br></br><br></br>
-                    No two releases will sound alike as no effort is put into following a specific template or routine. This is a DIY effort with a strong passion for the music and the scene.
-          <br></br><br></br>
-                    Twelve Gauge Records is exclusively distributed by <a href="http://deathwishdirect.com">Deathwish Direct</a>.
+                    <p>Twelve Gauge Records is an independent record label currently operating out of New York, NY with a rich roster of hardcore and metal artists like Sabertooth Zombie, Power, Heavy Heavy Low Low, and others.</p>
+
+                    <p>The label was founded in 2004 with it's first release, Courtside Seats EP by Heavy Heavy Low Low, out in early 2005. Since then, over 20 artists put together a discography of over 30 releases mostly pressed on vinyl only.</p>
+
+                    <p>No two releases will sound alike as no effort is put into following a specific template or routine. This is a DIY effort with a strong passion for the music and the scene.</p>
           </div>
 
             </div>
