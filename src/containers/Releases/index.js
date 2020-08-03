a4972f02e6647b187f33df047ff9d1bbc6452ab1
Sarah Rabah <sarahmrabah@gmail.com>
2020-08-03 16:46:41 -0400

    Sorts Releases based on release date > - Used to sort based on catalog number > - Catalog number does not always affect the order of the release since some releases can take longer than others - Allows release information to be added to the releaseData easily

--- src/containers/Releases/index.js
+++ src/containers/Releases/index.js
@@ -9,12 +9,17 @@ import releaseData from '../../assets/releaseData.js';
 class Releases extends Component {
     state = {}
 
+
     componentDidMount() {
+        let sortedReleases = releaseData.sort((x, y) => {
+            let a = new Date(x.release_date),
+                b = new Date(y.release_date);
+            return b - a;
+        });
         this.setState(
-            { releases: releaseData }
+            { releases: sortedReleases }
         )
     }
-
 
     // Renders release display grid with all releases if no item is selected
     render() {
@@ -27,6 +32,7 @@ class Releases extends Component {
                     <div className="sectionHeader">// Releases</div>
                         {this.state.releases.map(
                             (release, index) => {
+                                console.log(release.release_date)
                                 // Filter out unannounced releases
                                 if (release.status != 'Unannounced') {
                                     return (
