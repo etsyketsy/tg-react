a44acec9a4887505ee0decbb9ac53c1066eb7a80
Sarah Rabah <sarahmrabah@gmail.com>
2020-08-31 15:11:49 -0400

    Adds Donations component with existing iframe

---
+++ src/containers/Donations/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Donations extends Component {
+
+    render() {
+
+        return (
+            <div id='donations'>
+                <iframe 
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTfS9ejWL_RVw_8RT_1bTUGKA2-1ZtHeqA__oguR2LrdvUm0SWw1xdqqlOQlfQuA547-q8hOYo2X7FE/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" 
+                    width="90%" 
+                    height="640px"
+                >
+                </iframe>
+            </div>
+        )
+    }
+}
+
+export default Donations;
