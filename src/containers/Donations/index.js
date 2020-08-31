233ea651e807b1dbc4beb3356f94cf30f1ec7273
Sarah Rabah <sarahmrabah@gmail.com>
2020-08-31 15:15:49 -0400

    Adds Donations route to top-level index.js

--- src/containers/Donations/index.js
+++ src/containers/Donations/index.js
@@ -10,7 +10,7 @@ class Donations extends Component {
                 <iframe 
                     src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTfS9ejWL_RVw_8RT_1bTUGKA2-1ZtHeqA__oguR2LrdvUm0SWw1xdqqlOQlfQuA547-q8hOYo2X7FE/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" 
                     width="90%" 
-                    height="640px"
+                    height="400px"
                 >
                 </iframe>
             </div>
