b50f2d35545d5d7fd2320f8ec3db3e823d4dff7f
Sarah Rabah <sarahmrabah@gmail.com>
2020-08-31 15:18:39 -0400

    Adds sectino header, slight tweaks to sizing

--- src/containers/Donations/index.js
+++ src/containers/Donations/index.js
@@ -7,10 +7,11 @@ class Donations extends Component {
 
         return (
             <div id='donations'>
+                <div className="sectionHeader">{'//'} Donations</div>
                 <iframe 
                     src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTfS9ejWL_RVw_8RT_1bTUGKA2-1ZtHeqA__oguR2LrdvUm0SWw1xdqqlOQlfQuA547-q8hOYo2X7FE/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" 
-                    width="90%" 
-                    height="400px"
+                    width="100%" 
+                    height="350px"
                 >
                 </iframe>
             </div>
